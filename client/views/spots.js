7ee8cec42fc6daa98d2febe8ede79fb2ab459856
Blouze <blouze@gmail.com>
2014-11-09 17:16:20 +0100

    Phonegap plugins

--- client/views/spots.js
+++ client/views/spots.js
@@ -4,6 +4,39 @@ Template.spotsList.helpers({
 	}
 });
 
+Template.newSpot.rendered = function () {
+	navigator.geolocation.getCurrentPosition(function (position) {
+		alert('Latitude: '          + position.coords.latitude          + '\n' +
+			'Longitude: '         + position.coords.longitude         + '\n' +
+			'Altitude: '          + position.coords.altitude          + '\n' +
+			'Accuracy: '          + position.coords.accuracy          + '\n' +
+			'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
+			'Heading: '           + position.coords.heading           + '\n' +
+			'Speed: '             + position.coords.speed             + '\n' +
+			'Timestamp: '         + position.timestamp                + '\n');
+	}, function (err) {
+		alert('code: '    + err.code    + '\n' +
+			'message: ' + err.message + '\n');
+	});
+};
+
+Template.newSpot.helpers({
+	spotPhotoSrc: function () {
+		return Session.get("spotPhoto");
+	}
+});
+
+Template.newSpot.events({
+	'click button#cameraBtn': function () {
+		MeteorCamera.getPicture({}, function (err, data) {
+			if (!err)
+				Session.set("spotPhoto", data);
+			else
+				console.log(err.message);
+		});
+	}
+});
+
 AutoForm.addHooks(['insertSpotForm', 'updateSpotForm'], {
 	// Called when any operation succeeds, where operation will be
 	// "insert", "update", or the method name.
